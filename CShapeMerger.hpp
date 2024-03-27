0c1ea6f201763e7dbf3559616134ce5e387a9449
Jakub <imejlgrucha@gmail.com>
2024-03-27 01:13:30 +0100

    Cleanup code

--- CShapeMerger.hpp
+++ CShapeMerger.hpp
@@ -4,9 +4,6 @@
 class ShapeMerger
 {
 public:
-    // if a funcion that need more then 1 argument of each type you will need to make more refs
-    Triangle& triangle;
-    Circle& circle;
     ShapeMerger();
     void calculateFirstVertex (Triangle& triangle, Circle& circle);
     void calculateSecondVertex(Triangle& triangle, Circle& circle);
--- main.cpp
+++ main.cpp
@@ -6,31 +6,29 @@
 #include <vector>
 int main()
 {
+    Circle MyCircle(45);
     std::vector<Triangle> MyTriangles;
+
     MyTriangles.push_back(Triangle(HORIZONTAL, DOWN,   20,  40));
     MyTriangles.push_back(Triangle(VERTICAL,   LEFT,   20,  40));
     MyTriangles.push_back(Triangle(HORIZONTAL, UP,     20,  40));
     MyTriangles.push_back(Triangle(VERTICAL,   RIGHT,  20,  40));
-    Circle MyCircle(45);
+    
     ShapeMerger MyMerger;
     ShapeWriter MyWriter;
 
     std::vector<std::string> MyColors = {"red", "blue", "green", "black"};
-    int i = 0;
-    for (auto triangle : MyTriangles)
-    {
+
+    for (auto& triangle : MyTriangles) {
         MyMerger.calculateFirstVertex(triangle, MyCircle);
         MyMerger.calculateSecondVertex(triangle, MyCircle);
         MyMerger.calculateThirdVertex(triangle, MyCircle);
-        std::cout << "new point " << triangle.VertexA.getPoint() << std::endl;  
     }
-    for (auto triangle : MyTriangles)
-    {
-        std::cout << "one more time " << triangle.VertexA.getPoint() << std::endl;  
-    }
+
     MyWriter.CreateNewFile();
     MyWriter.CreateStartTag();
-    MyWriter.CreateTagFromShape(&MyTriangles.at(1));
+    for (auto& triangle : MyTriangles)
+        MyWriter.CreateTagFromShape(triangle);
     MyWriter.CreateExitTag();
     MyWriter.WriteToFile();
     std::cin.get();
